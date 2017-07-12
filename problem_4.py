2d556bbb934ddc64276ce31388552adcbcaab397
SedaByrk <sd.byrk@hotmail.com>
2017-07-12 15:33:18 +0300

    Yeni projeler eklendi.

---
+++ problem_4.py
@@ -0,0 +1,8 @@
+numb = []
+for i in range(999, 99, -1):
+    for j in range(999, 99, -1):
+        multiple = i * j
+        mult = str(multiple)
+        if(mult[0:] == mult[::-1]):
+            numb.append(int(mult))
+print(max(numb))
