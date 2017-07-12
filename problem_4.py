33196b1b586a3e76c39319328ac1249bf2ea4703
SedaByrk <sd.byrk@hotmail.com>
2017-07-12 15:53:48 +0300

    Fazla dosyalar silindi.

--- problem_4.py
+++
@@ -1,8 +0,0 @@
-numb = []
-for i in range(999, 99, -1):
-    for j in range(999, 99, -1):
-        multiple = i * j
-        mult = str(multiple)
-        if(mult[0:] == mult[::-1]):
-            numb.append(int(mult))
-print(max(numb))
